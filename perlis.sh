fb864d6257086edf90c0dd3a83cea617859989db
cucuatok93 <35328742+cucuatok93@users.noreply.github.com>
2018-01-13 19:33:11 +0800

    Update perlis.sh

--- perlis.sh
+++ perlis.sh
@@ -227,6 +227,17 @@ wget $source/pptp.sh
 chmod +x pptp.sh
 ./pptp.sh
 
+# install ssl
+apt-get update
+apt-get upgrade
+apt-get install stunnel4
+wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/cucuatok93/cucunenek/master/stunnel.conf"
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
 #menu
 wget -O /etc/motd $source/motd
 wget -O /etc/issue.net $source/banner
