a559c8a4b81a132c525375807f550fb20ec1cc79
cucuatok93 <35328742+cucuatok93@users.noreply.github.com>
2018-01-20 07:24:26 +0000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -290,6 +290,28 @@ User Sudah Boleh Multi Login Lagi!!!" | boxes -d boy | lolcat
 	;;
 	
 	25)
+	"Clear Cache Ram")
+	clear
+	echo "---------------------------------------------"
+	echo "Before..." | lolcat
+	echo "---------------------------------------------"
+       free -h
+	echo 1 > /proc/sys/vm/drop_caches
+	sleep 1
+	echo 2 > /proc/sys/vm/drop_caches
+	sleep 1
+	echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a
+	sleep 1
+	echo "---------------------------------------------"
+	echo "After..." | lolcat
+	echo "---------------------------------------------"
+	free -h
+	echo "---------------------------------------------"
+	echo "Success..!!!Cache ram anda sudah di bersihkan." | boxes -d boy | lolcat
+        echo ""
+	break
+	
+	26)
 		clear
 		exit
 		;;
